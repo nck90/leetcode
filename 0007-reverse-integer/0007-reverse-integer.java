f9e8e77d8e324a0044600339c6b59ed426d2f5c8
박준원 <162806035+nck90@users.noreply.github.com>
2024-09-26 23:45:01 +0900

    Time: 1 ms (84.58%), Space: 41.1 MB (25.86%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,17 +1,18 @@
 class Solution {
     public int reverse(int x) {
-        int n = 0;
-
+        int result = 0;
+        
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
             
-            if (n > Integer.MAX_VALUE / 10 || (n == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (n < Integer.MIN_VALUE / 10 || (n == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            
-            n = n * 10 + pop;
-        }
+            if (result > 214748364 || (result == 214748364 && pop > 7)) return 0;
+            if (result < -214748364 || (result == -214748364 && pop < -8)) return 0;
 
-        return n;
+            
+            result = result * 10 + pop;
+        }
+        
+        return result;
     }
 }

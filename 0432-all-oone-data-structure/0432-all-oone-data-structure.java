9b9b5ddde3783543b600707f1eae238890eabda0
박준원 <162806035+nck90@users.noreply.github.com>
2024-09-29 23:16:41 +0900

    Time: 51 ms (77.76%), Space: 60.7 MB (47.06%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.java
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -1,69 +1,105 @@
 import java.util.*;
 
 public class AllOne {
-    private class Node {
+    private class Bucket {
         int count;
-        Set<String> keys = new HashSet<>();
-        Node prev, next;
-        Node(int count) { this.count = count; }
+        Set<String> keys;
+        Bucket prev, next;
+
+        Bucket(int count) {
+            this.count = count;
+            keys = new HashSet<>();
+        }
     }
 
-    private Map<String, Node> keyNodeMap = new HashMap<>();
-    private Node head = new Node(Integer.MIN_VALUE);
-    private Node tail = new Node(Integer.MAX_VALUE);
+    private Map<String, Bucket> keyBucketMap;
+    private Bucket head, tail;
 
     public AllOne() {
+        keyBucketMap = new HashMap<>();
+
+        head = new Bucket(Integer.MIN_VALUE);
+        tail = new Bucket(Integer.MAX_VALUE);
         head.next = tail;
         tail.prev = head;
     }
 
     public void inc(String key) {
-        Node curr = keyNodeMap.get(key);
-        if (curr != null) {
-            Node next = curr.next.count == curr.count + 1 ? curr.next : insertAfter(curr, new Node(curr.count + 1));
-            next.keys.add(key);
-            keyNodeMap.put(key, next);
-            curr.keys.remove(key);
-            if (curr.keys.isEmpty()) remove(curr);
+        if (keyBucketMap.containsKey(key)) {
+            Bucket currBucket = keyBucketMap.get(key);
+            Bucket nextBucket;
+
+            if (currBucket.next.count == currBucket.count + 1) {
+                nextBucket = currBucket.next;
+            } else {
+                nextBucket = new Bucket(currBucket.count + 1);
+                insertBucketAfter(nextBucket, currBucket);
+            }
+
+            nextBucket.keys.add(key);
+            keyBucketMap.put(key, nextBucket);
+
+            currBucket.keys.remove(key);
+            if (currBucket.keys.isEmpty()) {
+                removeBucket(currBucket);
+            }
         } else {
-            Node next = head.next.count == 1 ? head.next : insertAfter(head, new Node(1));
-            next.keys.add(key);
-            keyNodeMap.put(key, next);
+            Bucket firstBucket;
+
+            if (head.next.count == 1) {
+                firstBucket = head.next;
+            } else {
+                firstBucket = new Bucket(1);
+                insertBucketAfter(firstBucket, head);
+            }
+
+            firstBucket.keys.add(key);
+            keyBucketMap.put(key, firstBucket);
         }
     }
 
     public void dec(String key) {
-        Node curr = keyNodeMap.get(key);
-        if (curr == null) return;
-        if (curr.count == 1) {
-            keyNodeMap.remove(key);
-        } else {
-            Node prev = curr.prev.count == curr.count - 1 ? curr.prev : insertAfter(curr.prev, new Node(curr.count - 1));
-            prev.keys.add(key);
-            keyNodeMap.put(key, prev);
+        Bucket currBucket = keyBucketMap.get(key);
+        if (currBucket == null) return;
+
+        keyBucketMap.remove(key);
+        if (currBucket.count > 1) {
+            Bucket prevBucket;
+
+            if (currBucket.prev.count == currBucket.count - 1) {
+                prevBucket = currBucket.prev;
+            } else {
+                prevBucket = new Bucket(currBucket.count - 1);
+                insertBucketAfter(prevBucket, currBucket.prev);
+            }
+
+            prevBucket.keys.add(key);
+            keyBucketMap.put(key, prevBucket);
         }
-        curr.keys.remove(key);
-        if (curr.keys.isEmpty()) remove(curr);
+
+        currBucket.keys.remove(key);
+        if (currBucket.keys.isEmpty()) {
+            removeBucket(currBucket);
+        }
     }
 
     public String getMaxKey() {
-        return tail.prev != head ? tail.prev.keys.iterator().next() : "";
+        return tail.prev == head ? "" : tail.prev.keys.iterator().next();
     }
 
     public String getMinKey() {
-        return head.next != tail ? head.next.keys.iterator().next() : "";
+        return head.next == tail ? "" : head.next.keys.iterator().next();
     }
 
-    private Node insertAfter(Node node, Node newNode) {
-        newNode.prev = node;
-        newNode.next = node.next;
-        node.next.prev = newNode;
-        node.next = newNode;
-        return newNode;
+    private void insertBucketAfter(Bucket newBucket, Bucket prevBucket) {
+        newBucket.next = prevBucket.next;
+        newBucket.prev = prevBucket;
+        prevBucket.next.prev = newBucket;
+        prevBucket.next = newBucket;
     }
 
-    private void remove(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+    private void removeBucket(Bucket bucket) {
+        bucket.prev.next = bucket.next;
+        bucket.next.prev = bucket.prev;
     }
 }
